b8564172af0681ae04e59f1227577e6088b71f83
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2024-06-20 14:06:28 +0300

    Create App.vue

---
+++ weather/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="weather">
+    <div class="container">
+      
+      <div class="card weather-form">
+        <input type="text" class="weather-form__input"
+        placeholder="Enter city">
+        <button class="weather-form__btn">Search</button>
+      </div>
+
+      <div class="weather-info">
+        <div class="weather-info__text">
+          <p class="card">Kiev</p>
+          <p class="card">29°C</p>
+          <p class="card">Sunny</p>
+        </div>
+      </div>
+    
+    </div>
+
+    <div class="weather_bg">
+      <div>
+        <img src="" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
